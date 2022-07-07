6434524e07027dc2c75c93e20a003ce8b8cf6e9e
Riad Younes <riad.younes@hotmail.com>
2022-07-07 19:18:29 -0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "instance_ami" {
+  type    = string
+  default = "ami-09d56f8956ab235b3"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
